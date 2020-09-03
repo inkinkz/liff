f0ac53d2c9457b96497b0864abe6a137f34452b0
inkinkz <nithiphorn.y@skooldio.com>
2020-09-04 00:24:43 +0700

    added comment

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,31 @@
-const request = require('request-promise')
+const request = require('request-promise');
 
 // Verifying Access Token and Channel ID
 const json = await request.get({
   url: `https://api.line.me/oauth2/v2.1/verify?access_token=${accessToken}`,
-  json: true
-})
+  json: true,
+});
 
 // check if it matches our channel id
+// mismatch = possibly spam
 if (json.client_id !== CHANNEL_ID) {
-  return 401
+  return 401;
 }
-
 
 // Getting User Profile by Access Token
 const profile = await request.get({
-  url: "https://api.line.me/v2/profile",
+  url: 'https://api.line.me/v2/profile',
   headers: { Authorization: `Bearer ${accessToken}` },
-  json: true
-})
-
+  json: true,
+});
 
 // Revoke Access Token
 await request.post({
- url: "https://api.line.me/oauth2/v2.1/revoke",
- headers: { "Content-Type": "application/x-www-form-urlencoded" },
- form: {
-   access_token: `${accessToken}`,
-   client_id: CHANNEL_ID,
-   client_secret: CHANNEL_SECRET
- } 
-})
+  url: 'https://api.line.me/oauth2/v2.1/revoke',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  form: {
+    access_token: `${accessToken}`,
+    client_id: CHANNEL_ID,
+    client_secret: CHANNEL_SECRET,
+  },
+});
